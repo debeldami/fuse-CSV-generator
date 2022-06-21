774d0bd0f7a3069e036ac2c6b3e6d3957e985d87
debeldami <debeldamilola@gmail.com>
2022-06-21 14:21:12 +0100

    generate csv from fuse explorer done

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
 const express = require('express');
+const axios = require('axios');
+const ObjectsToCsv = require('objects-to-csv');
+
 const app = express();
 app.use(express.json());
-const { asyncHandler, getUnixTimestamp } = require('./utils');
+const { asyncHandler } = require('./utils');
 
 const PORT = 3000;
 
 app.get(
   '/',
   asyncHandler(async (req, res, next) => {
-    const { address, action, from, to } = req.body;
     let url;
 
-    console.log(`${address} ${action} ${from} ${to}`);
+    const { address, action, hash } = req.body;
 
-    if (!(address && action && from && to)) {
+    if (!(address && action)) {
       return res.status(400).json({
         status: 'error',
         message: 'Please specify all the necessary parameters',
       });
     }
 
-    const fromTimeStamp = getUnixTimestamp(from);
-    const toTimeStamp = getUnixTimestamp(to);
+    if (action === 'txlistinternal') {
+      url = `https://explorer.fuse.io/api?module=account&action=${action}&txhash=${hash}`;
+    } else {
+      url = `https://explorer.fuse.io/api?module=account&action=${action}&address=${address}`;
+      console.log(url);
+    }
 
-    res.status(200).json({
-      message: 'hellow world',
-    });
+    try {
+      const { result } = (await axios.get(url)).data;
+
+      console.log(result);
+
+      const csv = new ObjectsToCsv(result);
+
+      await csv.toDisk(`./${address.slice(0, 6)}_${action}_${Date.now()}.csv`);
+
+      res.json({
+        message: 'CSV generated',
+      });
+    } catch (error) {
+      res.json({
+        message: 'An error occur, check the information you provided',
+      });
+    }
   })
 );
 
--- utils.js
+++ utils.js
@@ -1,9 +1,2 @@
 exports.asyncHandler = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
-
-exports.getUnixTimestamp = (dateStr) => {
-  const date = new Date(dateStr);
-  console.log(date);
-
-  return Math.floor(date.getTime() / 1000);
-};
